cf5075755843f2e79af81e0bac630f3c57e7252f
Johnny Hicks <johnnydhicks@gmail.com>
2019-05-09 19:37:13 -0600

    Initial Commit

---
+++ Stars (iOSPT1)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stars (iOSPT1)
+//
+//  Created by Johnny Hicks on 5/9/19.
+//  Copyright © 2019 Johnny Hicks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
